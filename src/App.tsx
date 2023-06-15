87607db257f371e622bd7da08522aeb8006fe141
Anna Escolaro <annaescolaro@hotmail.com>
2023-06-15 17:18:32 -0300

    req 1 finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Search from './pages/search/search';
 
@@ -6,17 +6,17 @@ function App() {
   return (
     <>
       <p>Trybetunes</p>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={ <Login /> } />
-          <Route path="/search" element={ <Search /> } />
-          {/* <Route path="/search" element={ <Album /> } />
-          <Route path="/favorites" element={ <Favorites /> } />
-          <Route path="/profile" element={ <Profile /> } />
-          <Route path="/profile/edit" element={ <ProfileEdit /> } />
-          <Route path="/*" element={ <NotFound /> } /> */}
-        </Routes>
-      </BrowserRouter>
+      {/* <BrowserRouter> */}
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/search" element={ <Search /> } />
+        {/* <Route path="/search" element={ <Album /> } />
+        <Route path="/favorites" element={ <Favorites /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/profile/edit" element={ <ProfileEdit /> } />
+        <Route path="/*" element={ <NotFound /> } /> */}
+      </Routes>
+      {/* </BrowserRouter> */}
     </>
   );
 }
