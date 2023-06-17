3c2cf3a83d09d387731a8c8c34e6adb2ef824234
Anna Escolaro <annaescolaro@hotmail.com>
2023-06-17 17:56:09 -0300

    corrige lint req 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Search from './pages/search/search';
 import Album from './pages/album/album';
